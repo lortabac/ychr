7a4204d4cbfa491121ea2d521c56dcceb6b8108b
Lorenzo Tabacchini <lortabac@gmx.com>
2026-03-30 15:24:15 +0200

    Add REPL commands

--- app/Main.hs
+++ app/Main.hs
@@ -11,6 +11,7 @@ import System.Directory (XdgDirectory (..), createDirectoryIfMissing, getXdgDire
 import System.Exit (exitFailure)
 import System.FilePath (takeDirectory)
 import YCHR.Display (displayMsg)
+import YCHR.Parsed qualified as Parsed
 import YCHR.EndToEnd (CompiledProgram (..), Error, compileFiles, compileModules, runProgramWithGoal, runProgramWithQuery)
 import YCHR.Meta (metaHostCallRegistry)
 import YCHR.Pretty (prettyBindings, prettyQueryResult)
@@ -69,21 +70,56 @@ runRepl files = do
       createDirectoryIfMissing True (takeDirectory histFile)
       let CompiledProgram {exportMap = em} = prog
           constraintNames = nub [T.unpack n | (n, _) <- Map.keys em]
-          completions = [":quit", ":q"] ++ constraintNames
+          completions = [":quit", ":recompile", ":help", ":list_files", ":list_modules", ":list_declarations"] ++ constraintNames
           completeFunc = completeWord Nothing " ," $ \prefix ->
             return $ map (\n -> (simpleCompletion n) {isFinished = False}) $ filter (isPrefixOf prefix) completions
           settings = (defaultSettings :: Settings IO) {historyFile = Just histFile, complete = completeFunc}
-      runInputT settings (repl prog)
+      runInputT settings (repl files prog)
 
-repl :: CompiledProgram -> InputT IO ()
-repl prog = loop
+repl :: [FilePath] -> CompiledProgram -> InputT IO ()
+repl files prog = loop
   where
+    showHelp = do
+      outputStrLn "Commands:"
+      outputStrLn "  :help, :h              Show this help message"
+      outputStrLn "  :recompile, :r         Recompile the loaded files"
+      outputStrLn "  :list_files            List the compiled files"
+      outputStrLn "  :list_modules          List the compiled modules"
+      outputStrLn "  :list_declarations     List visible declarations"
+      outputStrLn "  :quit, :q              Exit the REPL"
+      loop
+    showFiles = do
+      mapM_ outputStrLn files
+      loop
+    showDeclarations = do
+      let decls = nub [(n, a) | (n, a) <- Map.keys prog.exportMap]
+      mapM_ (\(n, a) -> outputStrLn (T.unpack n ++ "/" ++ show a)) decls
+      loop
+    showModules = do
+      mapM_ (\(Parsed.Module {name = n}) -> outputStrLn (T.unpack n)) prog.allModules
+      loop
+    recompile = do
+      result <- liftIO $ case files of
+        [] -> pure (compileModules True [])
+        _ -> compileFiles True files
+      case result of
+        Left err -> do
+          outputStr (displayMsg err)
+          loop
+        Right prog' -> repl files prog'
     loop = do
       minput <- getInputLine "ychr> "
       case minput of
         Nothing -> return ()
         Just ":quit" -> return ()
         Just ":q" -> return ()
+        Just ":recompile" -> recompile
+        Just ":r" -> recompile
+        Just ":help" -> showHelp
+        Just ":h" -> showHelp
+        Just ":list_files" -> showFiles
+        Just ":list_modules" -> showModules
+        Just ":list_declarations" -> showDeclarations
         Just "" -> loop
         Just line -> do
           outcome <-
