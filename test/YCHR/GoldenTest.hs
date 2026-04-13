a2d28f65765b7d05ed0ec3ca0855a78ec9804ef1
Lorenzo Tabacchini <lortabac@gmx.com>
2026-04-13 15:48:50 +0200

    Introduce negative golden tests

--- test/YCHR/GoldenTest.hs
+++ test/YCHR/GoldenTest.hs
@@ -2,13 +2,15 @@
 
 module YCHR.GoldenTest (tests) where
 
-import Data.List (sort)
+import Data.Char (isSpace)
+import Data.List (isInfixOf, sort)
 import Data.Text qualified as T
 import Data.Text.IO qualified as TIO
-import System.Directory (listDirectory)
+import System.Directory (doesFileExist, listDirectory)
 import System.FilePath (dropExtension, takeExtension, (<.>), (</>))
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit (assertFailure, testCase, (@?=))
+import Test.Tasty.HUnit (assertBool, assertFailure, testCase, (@?=))
+import YCHR.Display (Display (..))
 import YCHR.EndToEnd (compileFiles, runProgramWithGoal)
 import YCHR.Meta (metaHostCallRegistry)
 import YCHR.Pretty (prettyBindings)
@@ -26,7 +28,23 @@ tests = do
   pure (testGroup "Golden" trees)
 
 makeGoldenTest :: FilePath -> String -> IO TestTree
-makeGoldenTest base name = pure $ testCase name $ do
+makeGoldenTest base name = do
+  let errorFile = base </> "expected" </> name <.> "error"
+      expectedFile = base </> "expected" </> name <.> "expected"
+  isNeg <- doesFileExist errorFile
+  isPos <- doesFileExist expectedFile
+  pure $ case (isPos, isNeg) of
+    (True, False) -> makePositiveTest base name
+    (False, True) -> makeNegativeTest base name
+    (True, True) ->
+      testCase name $
+        assertFailure "Both .expected and .error files exist"
+    (False, False) ->
+      testCase name $
+        assertFailure "Neither .expected nor .error file found"
+
+makePositiveTest :: FilePath -> String -> TestTree
+makePositiveTest base name = testCase name $ do
   (prog, _) <-
     compileFiles False [base </> "programs" </> name <.> "chr"]
       >>= either (assertFailure . show) pure
@@ -34,3 +52,18 @@ makeGoldenTest base name = pure $ testCase name $ do
   expected <- readFile (base </> "expected" </> name <.> "expected")
   (_, bindings) <- runProgramWithGoal prog (baseHostCallRegistry <> metaHostCallRegistry) (T.strip query)
   prettyBindings bindings @?= expected
+
+makeNegativeTest :: FilePath -> String -> TestTree
+makeNegativeTest base name = testCase name $ do
+  result <- compileFiles False [base </> "programs" </> name <.> "chr"]
+  expectedCode <- trim <$> readFile (base </> "expected" </> name <.> "error")
+  case result of
+    Left err -> do
+      let msg = displayMsg err
+      assertBool
+        ("Expected error code " ++ expectedCode ++ " in:\n" ++ msg)
+        (expectedCode `isInfixOf` msg)
+    Right _ ->
+      assertFailure "Expected compilation to fail, but it succeeded"
+  where
+    trim = reverse . dropWhile isSpace . reverse . dropWhile isSpace
